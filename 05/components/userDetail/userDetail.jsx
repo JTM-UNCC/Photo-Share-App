54ac1c7360b6ac92435dd3f5a4288841af8ec1b0
ZahraO1 <66895082+ZahraO1@users.noreply.github.com>
2023-10-13 10:19:50 -0400

    updated userList.jsx and userDetail.jsx

--- 05/components/userDetail/userDetail.jsx
+++ 05/components/userDetail/userDetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
-  Typography
+    Divider, List, ListItem, ListItemText,
+    Typography
 } from '@mui/material';
 import './userDetail.css';
 
@@ -11,17 +12,39 @@ import './userDetail.css';
 class UserDetail extends React.Component {
   constructor(props) {
     super(props);
+    this.user = {
+        detail:window.models.userModel(this.props.match.params.userId)
+    }
   }
 
   render() {
     return (
-      <Typography variant="body1">
-        This should be the UserDetail view of the PhotoShare app. Since
-        it is invoked from React Router the params from the route will be
-        in property match. So this should show details of user:
-        {this.props.match.params.userId}. You can fetch the model for the
-        user from window.models.userModel(userId).
-      </Typography>
+        <div>
+            <Typography variant="body1">
+                This should be the UserDetail view of the PhotoShare app. Since
+                it is invoked from React Router the params from the route will be
+                in property match. So this should show details of user:
+                {this.props.match.params.userId}. You can fetch the model for the
+                user from window.models.userModel(userId).
+            </Typography>
+                <List>
+                    <ListItem>
+                        <ListItemText> {`Name: ${this.user.detail.first_name} ${this.user.detail.last_name}`}</ListItemText>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText>{`Location: ${this.user.detail.location}`}</ListItemText>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText>{`Description: ${this.user.detail.description}`}</ListItemText>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText>{`Occupation: ${this.user.detail.occupation}`}</ListItemText>
+                    </ListItem>
+                </List>
+                <Typography>
+                    Test
+                </Typography>
+        </div>
     );
   }
 }
--- 05/components/userList/userList.jsx
+++ 05/components/userList/userList.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {
   Divider,
   List,
-  ListItem,
+  ListItem, ListItemButton,
   ListItemText,
   Typography,
 }
-from '@mui/material';
+  from '@mui/material';
 import './userList.css';
+//import UserDetail from './components/userDetail/userDetail';
 
 /**
  * Define UserList, a React component of project #5
@@ -18,14 +19,16 @@ class UserList extends React.Component {
     this.user = {
       list:window.models.userListModel()
     }
-    this.handleClick = this.handleClick.bind(this);
+    //this.handleClick = this.handleClick.bind(this);
     console.log('window.models.userListModel', window.models.userListModel);
   }
 
+  /**
   handleClick(event) {
     alert('hello');
     alert(this.user.list[0]._id)
   }
+      */
 
   render() {
     return (
@@ -37,36 +40,46 @@ class UserList extends React.Component {
         </Typography>
         <List component="nav">
           <ListItem>
-            <ListItemText
-                id="0"
-                primary={`${this.user.list[0].first_name} ${this.user.list[0].last_name}`}
-                onClick = {(event) => this.handleClick(event)}
-            />
+            <ListItemButton href={`/photo-share.html#/users/${this.user.list[0]._id}`}>
+              <ListItemText
+                  primary={`${this.user.list[0].first_name} ${this.user.list[0].last_name}`}
+              />
+            </ListItemButton>
           </ListItem>
           <Divider />
 
           <ListItem>
-            <ListItemText primary={`${this.user.list[1].first_name} ${this.user.list[1].last_name}`} />
+            <ListItemButton href={`/photo-share.html#/users/${this.user.list[1]._id}`}>
+              <ListItemText primary={`${this.user.list[1].first_name} ${this.user.list[1].last_name}`} />
+            </ListItemButton>
           </ListItem>
           <Divider />
 
           <ListItem>
-            <ListItemText primary={`${this.user.list[2].first_name} ${this.user.list[2].last_name}`} />
+            <ListItemButton href={`/photo-share.html#/users/${this.user.list[2]._id}`}>
+              <ListItemText primary={`${this.user.list[2].first_name} ${this.user.list[2].last_name}`} />
+            </ListItemButton>
           </ListItem>
           <Divider />
 
           <ListItem>
-            <ListItemText primary={`${this.user.list[3].first_name} ${this.user.list[3].last_name}`} />
+            <ListItemButton href={`/photo-share.html#/users/${this.user.list[3]._id}`}>
+              <ListItemText primary={`${this.user.list[3].first_name} ${this.user.list[3].last_name}`} />
+            </ListItemButton>
           </ListItem>
           <Divider />
 
           <ListItem>
-            <ListItemText primary={`${this.user.list[4].first_name} ${this.user.list[4].last_name}`} />
+            <ListItemButton href={`/photo-share.html#/users/${this.user.list[4]._id}`}>
+              <ListItemText primary={`${this.user.list[4].first_name} ${this.user.list[4].last_name}`} />
+            </ListItemButton>
           </ListItem>
           <Divider />
 
           <ListItem>
-            <ListItemText primary={`${this.user.list[5].first_name} ${this.user.list[5].last_name}`} />
+            <ListItemButton href={`/photo-share.html#/users/${this.user.list[5]._id}`}>
+              <ListItemText primary={`${this.user.list[5].first_name} ${this.user.list[5].last_name}`} />
+            </ListItemButton>
           </ListItem>
           <Divider />
         </List>
